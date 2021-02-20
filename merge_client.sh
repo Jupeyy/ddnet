2a438f538ad7bf35df939485514c8b0d4d6b86ae
Jupeyy <jupjopjap@gmail.com>
2021-02-20 14:45:56 +0100

    Merge stuff

---
+++ merge_client.sh
@@ -0,0 +1,8 @@
+git fetch --all
+git reset --hard upstream/master 
+
+git merge origin/client_cmake
+git merge origin/client_quake
+git merge origin/pr_try_new_windows_stuff
+git merge origin/pr_fng_scoreboard
+git merge origin/pr_more_console_colors
