3b6a15739d23f7dc5eebdae2ebea743c0a715a44
Jupeyy <jupjopjap@gmail.com>
2021-09-15 23:52:09 +0200

    Add new mouse state

--- src/engine/client/backend_sdl.cpp
+++ src/engine/client/backend_sdl.cpp
@@ -900,9 +900,6 @@ int CGraphicsBackend_SDL_OpenGL::Init(const char *pName, int *pScreen, int *pWid
 		SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 0);
 	}
 
-	if(g_Config.m_InpMouseOld)
-		SDL_SetHint(SDL_HINT_MOUSE_RELATIVE_MODE_WARP, "1");
-
 	m_pWindow = SDL_CreateWindow(
 		pName,
 		SDL_WINDOWPOS_CENTERED_DISPLAY(*pScreen),
@@ -1249,6 +1246,11 @@ void CGraphicsBackend_SDL_OpenGL::NotifyWindow()
 	SndNtfyEvent.xclient.data.l[4] = 0;
 	XSendEvent(pX11Dpy, XDefaultRootWindow(pX11Dpy), False, SubstructureNotifyMask | SubstructureRedirectMask, &SndNtfyEvent);
 #endif
+}
+
+void CGraphicsBackend_SDL_OpenGL::WarpMouse(int MouseX, int MouseY)
+{
+	SDL_WarpMouseInWindow(m_pWindow, MouseX, MouseY);
 }
 
 IGraphicsBackend *CreateGraphicsBackend() { return new CGraphicsBackend_SDL_OpenGL; }
--- src/engine/client/graphics_threaded.cpp
+++ src/engine/client/graphics_threaded.cpp
@@ -2437,6 +2437,11 @@ void CGraphics_Threaded::NotifyWindow()
 	return m_pBackend->NotifyWindow();
 }
 
+void CGraphics_Threaded::WarpMouse(int MouseX, int MouseY)
+{
+	return m_pBackend->WarpMouse(MouseX, MouseY);
+}
+
 void CGraphics_Threaded::TakeScreenshot(const char *pFilename)
 {
 	// TODO: screenshot support
